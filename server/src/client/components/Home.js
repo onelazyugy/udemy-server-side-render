a891e14648f84e3c0501e63f641fa4e0cee0e606
viet <viettangle12@gmail.com>
2017-11-05 21:27:52 -0500

    routing on server and client

--- server/src/client/components/Home.js
+++ server/src/client/components/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <div>I'm the very very BEST home component</div>
+            <div>I'm the home component</div>
             <button onClick={() => console.log('Hi there!')}>Press me!</button>
         </div>
     );
--- server/webpack.server.js
+++ server/webpack.server.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const merge = require('webpack-merge');
 const baseConfig = require('./webpack.base.js');
+const webpackNodeExternals = require('webpack-node-externals');
 
 const config = {
     // Inform webpack that we're building a bundle for nodeJS, rather for the browser [browser would not see this]
@@ -11,7 +12,10 @@ const config = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'build')
-    }
+    },
+
+    // tell webpack to not bundle any library from node_modules onto the server side bundle.js
+    externals: [webpackNodeExternals()]
 };
 
 module.exports = merge(baseConfig, config);
